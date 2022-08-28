d1892b2182dc5961a5ccadb791ba28c3d12fa021
mvbleal <mvinileal@outlook.com>
2022-08-28 19:26:20 -0400

    My third commit

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+/*
+Marcus Leal
+Aprendendo-a-programar
+Ituring
+*/
+const multiplicador = 6
+let i = 1
+
+while (i <= 10) {
+    const resultado = i*multiplicador
+    const valorExibido = `${multiplicador} X ${i} = ${resultado}`
+    console.log(valorExibido)
+    i++
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,8 @@
+const n = 16 
+let i = 1
+
+while (i <= n) {
+    console.log(i)
+    i += 2 
+}
+
